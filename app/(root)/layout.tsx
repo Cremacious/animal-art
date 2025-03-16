f0b3eefae4105b450eb005c083af63cc14610079
Cremacious <chrismackall3@gmail.com>
2025-03-16 17:45:19 -0400

    Restyled nav bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import Header from '@/components/shared/header';
+// import Header from '@/components/shared/header';
 
 // import Footer from '@/components/footer';
+
+import { Navbar1 } from '@/components/shared/header';
 
 export default function RootLayout({
   children,
@@ -11,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">
-      <Header />
+      <Navbar1 />
       <div className="flex flex-1 overflow-hidden">
         <main className="flex-1 wrapper relative z-10">{children}</main>
       </div>
