8fc8afa2907b054774e4a88c3274a01d89a3411d
Cremacious <chrismackall3@gmail.com>
2025-03-24 21:44:47 -0400

    Added callback to sign in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,16 +1,21 @@
+import { Metadata } from 'next';
 import SignInForm from './sign-in-form';
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 
-const SignInPage = async () => {
+export const metadata: Metadata = {
+  title: 'Sign in',
+};
 
-
+const SignInPage = async (props: {
+  searchParams: Promise<{ callbackUrl: string }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
   const session = await auth();
 
   if (session) {
-    redirect('/');
+    return redirect(callbackUrl || '/');
   }
-
   return (
     <div className="flex min-h-screen w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
