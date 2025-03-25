f425b56a6e1deeb71c21a84877f015e898216712
Cremacious <chrismackall3@gmail.com>
2025-03-25 11:51:51 -0400

    Prices in checkout are now reflected

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -58,54 +58,54 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
                         </h3>
                       </div>
                       <div className="mt-auto flex items-center gap-3">
-                        <button
+                        {/* Button */}
+                        <Button
+                          disabled={isPending}
+                          variant="outline"
                           type="button"
-                          className="flex items-center justify-center w-5 h-5 bg-slate-400 outline-none rounded-full"
+                          onClick={() =>
+                            startTransition(async () => {
+                              const res = await removeItemFromCart(
+                                item.productId
+                              );
+                              if (!res.success) {
+                                toast.error(res.message);
+                              }
+                            })
+                          }
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-2 fill-white"
-                            viewBox="0 0 124 124"
-                          >
-                            <path
-                              d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z"
-                              data-original="#000000"
-                            />
-                          </svg>
-                        </button>
-                        <span className="font-semibold text-sm leading-[18px]">
-                          2
-                        </span>
-                        <button
+                          {isPending ? (
+                            <Loader className="w-4 h-4  animate-spin" />
+                          ) : (
+                            <Minus className="w-4 h-4" />
+                          )}
+                        </Button>
+                        <span>{item.quantity}</span>
+                        <Button
+                          disabled={isPending}
+                          variant="outline"
                           type="button"
-                          className="flex items-center justify-center w-5 h-5 bg-slate-800 outline-none rounded-full"
+                          onClick={() =>
+                            startTransition(async () => {
+                              const res = await addItemToCart(item);
+                              if (!res.success) {
+                                toast.error(res.message);
+                              }
+                            })
+                          }
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-2 fill-white"
-                            viewBox="0 0 42 42"
-                          >
-                            <path
-                              d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"
-                              data-original="#000000"
-                            />
-                          </svg>
-                        </button>
+                          {isPending ? (
+                            <Loader className="w-4 h-4  animate-spin" />
+                          ) : (
+                            <Plus className="w-4 h-4" />
+                          )}
+                        </Button>
                       </div>
                     </div>
                   </div>
                   <div className="ml-auto flex flex-col">
                     <div className="flex items-start gap-4 justify-end">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-4 h-4 cursor-pointer fill-slate-400 hover:fill-pink-600 inline-block"
-                        viewBox="0 0 64 64"
-                      >
-                        <path
-                          d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                          data-original="#000000"
-                        />
-                      </svg>
+                      {/* Trashcan */}
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="w-4 h-4 cursor-pointer fill-slate-400 hover:fill-red-600 inline-block"
@@ -309,32 +309,50 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
               <ul className="text-slate-900 font-medium space-y-4">
                 <li className="flex flex-wrap gap-4 text-sm">
                   Subtotal{' '}
-                  <span className="ml-auto font-semibold">$200.00</span>
+                  <span className="ml-auto font-semibold">
+                    {formatCurrency(cart.itemsPrice)}
+                  </span>
                 </li>
                 <li className="flex flex-wrap gap-4 text-sm">
-                  Shipping <span className="ml-auto font-semibold">$2.00</span>
+                  Shipping{' '}
+                  <span className="ml-auto font-semibold">
+                    ${cart.shippingPrice}
+                  </span>
                 </li>
                 <li className="flex flex-wrap gap-4 text-sm">
-                  Tax <span className="ml-auto font-semibold">$4.00</span>
+                  Tax{' '}
+                  <span className="ml-auto font-semibold">
+                    ${cart.taxPrice}
+                  </span>
                 </li>
                 <hr className="border-slate-300" />
                 <li className="flex flex-wrap gap-4 text-sm font-semibold">
-                  Total <span className="ml-auto">$206.00</span>
+                  Total <span className="ml-auto">${cart.totalPrice}</span>
                 </li>
               </ul>
               <div className="mt-8 space-y-2">
-                <button
-                  type="button"
+                <Button
+                  onClick={() =>
+                    startTransition(() => router.push('/shipping-address'))
+                  }
                   className="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-slate-800 hover:bg-slate-900 text-white rounded-md"
+                  disabled={isPending}
                 >
-                  Buy Now
-                </button>
-                <button
+                  {isPending ? (
+                    <Loader className="animate-spin w-4 h-4" />
+                  ) : (
+                    <ArrowRight className="w-4 h-4" />
+                  )}
+                  Proceed to Checkout
+                </Button>
+
+                <Button
+                  onClick={() => router.push('/search')}
                   type="button"
                   className="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-transparent hover:bg-slate-100 text-slate-900 border border-slate-300 rounded-md"
                 >
-                  Continue Shopping{'{'}' '{'}'}
-                </button>
+                  Continue Shopping
+                </Button>
               </div>
               <div className="mt-4 flex flex-wrap justify-center gap-4">
                 <img
