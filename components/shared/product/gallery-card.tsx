78ca9b45000d4483e8e754d1ff9784371f28e018
Cremacious <chrismackall3@gmail.com>
2025-03-24 21:27:39 -0400

    Fixed gallery card view button

--- components/shared/product/gallery-card.tsx
+++ components/shared/product/gallery-card.tsx
@@ -1,7 +1,5 @@
-import { Card, CardContent, CardFooter } from '@/components/ui/card';
-import { Eye, EyeIcon } from 'lucide-react';
-
 import { Button } from '@/components/ui/button';
+import { EyeIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Product } from '@/types';
@@ -9,8 +7,7 @@ import { Product } from '@/types';
 const GalleryCard = ({ product }: { product: Product }) => {
   return (
     <div className="flex flex-col items-center">
-      <Card className="w-[300px] group relative space-y-4 overflow-hidden rounded-lg shadow-md hover:shadow-lg  transition-shadow">
-        {/* Product Image */}
+      <div className="w-[300px] group relative space-y-4 overflow-hidden rounded-lg shadow-md hover:shadow-lg transition-shadow bg-white">
         <figure className="relative group-hover:opacity-90 transition-opacity">
           <Image
             className="aspect-square w-full object-cover p-2"
@@ -20,9 +17,7 @@ const GalleryCard = ({ product }: { product: Product }) => {
             alt={product.name}
           />
         </figure>
-
-        {/* Product Details */}
-        <CardContent className="px-4 py-2">
+        <div className="px-4 py-2">
           <div className="flex justify-between items-start">
             <div>
               <h3 className="text-lg font-semibold">
@@ -31,27 +26,22 @@ const GalleryCard = ({ product }: { product: Product }) => {
                   {product.name}
                 </Link>
               </h3>
-              <p className="text-sm text-muted-foreground">
-                {product.description}
-              </p>
+              <p className="text-sm text-gray-500">{product.description}</p>
             </div>
             <p className="bg-teal-300 text-lg rounded-2xl p-2 font-semibold text-black">
               ${product.price}
             </p>
           </div>
-        </CardContent>
-        <div className="flex justify-center items-center mt-2 border-t pt-2">
+        </div>
+        <div className="flex justify-center items-center mt-2 border-t">
           <Link href={`/products/${product.slug}`}>
-            <Button
-              variant="ghost"
-              className="bg-teal-200 w-full flex items-center justify-center"
-            >
+            <Button className="bg-teal-200 my-2 text-black w-full flex items-center justify-center gap-2 px-4 ">
               <EyeIcon className="w-4 h-4" />
               View
             </Button>
           </Link>
         </div>
-      </Card>
+      </div>
     </div>
   );
 };
