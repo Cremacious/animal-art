4d817d1cbee34a10ca4cc96a49bd58caa734b35d
Cremacious <chrismackall3@gmail.com>
2025-04-01 14:16:12 -0400

    Added margin and removed unused import

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -37,100 +37,104 @@ const AdminOverviewPage = async () => {
   const summary = await getOrderSummary();
 
   return (
-    <div className="space-y-2 bg-gray-200 rounded-2xl container mx-auto py-4">
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 px-2">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-            <BadgeDollarSign />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {formatCurrency(summary.totalSales._sum.totalPrice!.toString())}
-            </div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Sales</CardTitle>
-            <CreditCard />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {formatLocalNumber(summary.orderCount)}
-            </div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Customers</CardTitle>
-            <Users />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{summary.usersCount}</div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Products</CardTitle>
-            <Barcode />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{summary.productsCount}</div>
-          </CardContent>
-        </Card>
-      </div>
-      <div className="flex gap-2 flex-col md:flex-row justify-evenly mx-2">
-        <Card className="w-full">
-          <CardHeader>
-            <CardTitle>Overview</CardTitle>
-          </CardHeader>
-          <CardContent className="pl-2">
-            <Charts
-              data={{
-                salesData: summary.salesData,
-              }}
-            />
-          </CardContent>
-        </Card>
-        <Card className="">
-          <CardHeader>
-            <CardTitle>Recent Sales</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>BUYER</TableHead>
-                  <TableHead>DATE</TableHead>
-                  <TableHead>TOTAL</TableHead>
-                  <TableHead>ACTIONS</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {summary.latestSales.map((order) => (
-                  <TableRow key={order.id}>
-                    <TableCell>
-                      {order.user?.name ? order.user.name : 'Deleted user'}
-                    </TableCell>
-                    <TableCell>
-                      {formatDateTime(order.createdAt).dateOnly}
-                    </TableCell>
-                    <TableCell>
-                      {formatCurrency(order.totalPrice.toString())}
-                    </TableCell>
-                    <TableCell>
-                      <Link href={`/order/${order.id}`}>
-                        <span className="px-2">Details</span>
-                      </Link>
-                    </TableCell>
+    <div className="px-2">
+      <div className="space-y-2 bg-gray-200 rounded-2xl container mx-auto py-4">
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 px-2">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <BadgeDollarSign />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatCurrency(summary.totalSales._sum.totalPrice!.toString())}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CreditCard />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatLocalNumber(summary.orderCount)}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Customers</CardTitle>
+              <Users />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{summary.usersCount}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Products</CardTitle>
+              <Barcode />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{summary.productsCount}</div>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="flex gap-2 flex-col md:flex-row justify-evenly mx-2">
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle>Overview</CardTitle>
+            </CardHeader>
+            <CardContent className="pl-2">
+              <Charts
+                data={{
+                  salesData: summary.salesData,
+                }}
+              />
+            </CardContent>
+          </Card>
+          <Card className="">
+            <CardHeader>
+              <CardTitle>Recent Sales</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>BUYER</TableHead>
+                    <TableHead>DATE</TableHead>
+                    <TableHead>TOTAL</TableHead>
+                    <TableHead>ACTIONS</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
-        <div className="lg:col-span-1"></div>
+                </TableHeader>
+                <TableBody>
+                  {summary.latestSales.map((order) => (
+                    <TableRow key={order.id}>
+                      <TableCell>
+                        {order.user?.name ? order.user.name : 'Deleted user'}
+                      </TableCell>
+                      <TableCell>
+                        {formatDateTime(order.createdAt).dateOnly}
+                      </TableCell>
+                      <TableCell>
+                        {formatCurrency(order.totalPrice.toString())}
+                      </TableCell>
+                      <TableCell>
+                        <Link href={`/order/${order.id}`}>
+                          <span className="px-2">Details</span>
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+          <div className="lg:col-span-1"></div>
+        </div>
       </div>
     </div>
   );
--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,4 +1,4 @@
-import MainNavbar from '@/components/shared/header/MainNavbar';
+
 
 export default function RootLayout({
   children,
@@ -7,11 +7,11 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex flex-col">
-      <MainNavbar />
+
       <div className="flex flex-1 overflow-hidden">
         <main className="flex-1 wrapper relative z-10">{children}</main>
       </div>
-      {/* <Footer /> */}
+
     </div>
   );
 }
