b3ae01a7cc486725bee83999ed562140b344c294
Cremacious <chrismackall3@gmail.com>
2025-04-02 13:43:59 -0400

    Fixed background on orders page

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -38,56 +38,60 @@ const OrdersPage = async (props: {
   console.log(orders);
 
   return (
-    <div className="space-y-2">
-      <h2 className="h2-bold">Orders</h2>
-      <div className="overflow-x-auto">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>ID</TableHead>
-              <TableHead>DATE</TableHead>
-              <TableHead>TOTAL</TableHead>
-              <TableHead>PAID</TableHead>
-              <TableHead>DELIVERED</TableHead>
-              <TableHead>ACTIONS</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {orders.data.map((order) => (
-              <TableRow key={order.id}>
-                <TableCell>{formatId(order.id)}</TableCell>
-                <TableCell>
-                  {formatDateTime(order.createdAt).dateTime}
-                </TableCell>
-                <TableCell>
-                  {formatCurrency(order.totalPrice.toString())}
-                </TableCell>
-                <TableCell>
-                  {order.isPaid && order.paidAt
-                    ? formatDateTime(order.paidAt).dateTime
-                    : 'Not Paid'}
-                </TableCell>
-                <TableCell>
-                  {order.isDelivered && order.deliveredAt
-                    ? formatDateTime(order.deliveredAt).dateTime
-                    : 'Not Delivered'}
-                </TableCell>
-                <TableCell className="flex gap-1">
-                  <Button asChild variant="outline" size="sm">
-                    <Link href={`/order/${order.id}`}>Details</Link>
-                  </Button>
-                  <DeleteDialog id={order.id} action={deleteOrder} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        {orders.totalPages > 1 && (
-          <Pagination
-            page={Number(page) || 1}
-            totalPages={orders?.totalPages}
-          />
-        )}
+    <div className="flex justify-center p-2">
+      <div className="bg-white container min-h-screen rounded-2xl p-4">
+        <div className="space-y-2">
+          <h2 className="h2-bold">Orders</h2>
+          <div className="overflow-x-auto">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>ID</TableHead>
+                  <TableHead>DATE</TableHead>
+                  <TableHead>TOTAL</TableHead>
+                  <TableHead>PAID</TableHead>
+                  <TableHead>DELIVERED</TableHead>
+                  <TableHead>ACTIONS</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {orders.data.map((order) => (
+                  <TableRow key={order.id}>
+                    <TableCell>{formatId(order.id)}</TableCell>
+                    <TableCell>
+                      {formatDateTime(order.createdAt).dateTime}
+                    </TableCell>
+                    <TableCell>
+                      {formatCurrency(order.totalPrice.toString())}
+                    </TableCell>
+                    <TableCell>
+                      {order.isPaid && order.paidAt
+                        ? formatDateTime(order.paidAt).dateTime
+                        : 'Not Paid'}
+                    </TableCell>
+                    <TableCell>
+                      {order.isDelivered && order.deliveredAt
+                        ? formatDateTime(order.deliveredAt).dateTime
+                        : 'Not Delivered'}
+                    </TableCell>
+                    <TableCell className="flex gap-1">
+                      <Button asChild variant="outline" size="sm">
+                        <Link href={`/order/${order.id}`}>Details</Link>
+                      </Button>
+                      <DeleteDialog id={order.id} action={deleteOrder} />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            {orders.totalPages > 1 && (
+              <Pagination
+                page={Number(page) || 1}
+                totalPages={orders?.totalPages}
+              />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
